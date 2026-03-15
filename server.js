6a201766e68f0da210efee64195114fb62fd3091
nvanory <n.vanory@gmail.com>
2026-03-15 11:56:44 +0300

    server.js

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     socket.on('join_room', (roomId) => { socket.join(roomId); });
+    
     socket.on('play_video', (data) => { socket.to(data.roomId).emit('play_video', data.time); });
     socket.on('pause_video', (roomId) => { socket.to(roomId).emit('pause_video'); });
+    
+    // Новое: обработка перемотки видео
+    socket.on('seek_video', (data) => { socket.to(data.roomId).emit('seek_video', data.time); });
+    
     socket.on('change_video', (data) => { socket.to(data.roomId).emit('change_video', data.videoUrl); });
 });
 
