5c31e4c98c19c2844da997df49089391d09b45fa
nvanory <n.vanory@gmail.com>
2026-03-15 11:41:53 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('public'));
+
+io.on('connection', (socket) => {
+    socket.on('join_room', (roomId) => { socket.join(roomId); });
+    socket.on('play_video', (data) => { socket.to(data.roomId).emit('play_video', data.time); });
+    socket.on('pause_video', (roomId) => { socket.to(roomId).emit('pause_video'); });
+    socket.on('change_video', (data) => { socket.to(data.roomId).emit('change_video', data.videoUrl); });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Сервер запущен на порту ${PORT}`);
+});
